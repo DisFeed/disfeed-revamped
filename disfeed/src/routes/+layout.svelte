03168840121944b2a827f8290bee0a26fe86a7e6
DisFeed <47443853+DisFeed@users.noreply.github.com>
2024-08-01 13:19:01 +1000

    Tailwind and partial navbar completion

---
+++ disfeed/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<div class="bg-black size-1 w-full px-80 py-5 h-12 flex justify-center">
+    <div class="flex w-full justify-between items-center">
+        <div>Bruh</div>
+        <div>Bruh</div>
+        <div>Bruh</div>
+        <button class="btn btn-primary">Start</button>
+    </div>
+</div>
+
+<script>
+    import "../app.css";
+  </script>
+  
+  <slot />
--- disfeed/tailwind.config.js
+++ disfeed/tailwind.config.js
@@ -4,6 +4,33 @@ export default {
   theme: {
     extend: {},
   },
+
+  daisyui: {
+    themes: [
+      {
+        mainTheme: {
+          "primary": "#ffffff",
+                    
+          "secondary": "#4b5563",
+                    
+          "accent": "#1acb49",
+                    
+          "neutral": "#1c1917",
+                    
+          "base-100": "#181818",
+                    
+          "info": "#00b6ff",
+                    
+          "success": "#00a96e",
+                    
+          "warning": "#ffbe00",
+                    
+          "error": "#ff5861",
+          },
+        },
+      ],
+    },
+
   plugins: [require('daisyui')],
 }
 
